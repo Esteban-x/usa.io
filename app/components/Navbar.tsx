48b177b24598caac3674de59ff135561e4f9f245
Esteban-x <esteprowork@gmail.com>
2024-02-16 12:03:53 +0100

    Refactor code for improved performance and readability

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -17,11 +17,11 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex-1 flex items-center justify-center">
-            <div className="flex  items-center shadow-sm dark:bg-[#202F47] bg-white rounded-full px-6 fold:px-3 se:px-4 se:space-x-2 lg:px-6 2xl:px-14 xl:px-10 md:px-6 sm:px-6 py-1 md:space-x-5 lg:space-x-7 xl:space-x-7 2xl:space-x-8 ">
+            <div className="flex  items-center shadow-sm  bg-white rounded-full px-6 fold:px-3 se:px-4 se:space-x-2  lg:px-6 big:px-16 2xl:px-14 xl:px-10 md:px-6 sm:px-6 py-1 md:space-x-5 lg:space-x-7 xl:space-x-7 2xl:space-x-7 big:space-x-7 ">
               <Link href="/">
                 <div className="shrink-0 cursor-pointer">
                   <Image
-                    className="dark:opacity-85 xl:w-8 lg:w-8  md:w-8 w-6 mr-2 h-auto sm:w-6"
+                    className=" xl:w-8 lg:w-8  md:w-8 w-6 mr-2 h-auto sm:w-6"
                     src="/flag.svg"
                     width={40}
                     height={40}
@@ -31,14 +31,14 @@ const Navbar = () => {
               </Link>
               {links.map((link, index) => (
                 <Link
-                  className={`${currentRoute === link.path ? 'text-black/60 dark:text-white' : 'text-black/30 dark:text-gray-300'}  transition-colors hover:text-black/60 se:px-2 sm:px-3 md:px-3 lg:px-3 xl:px-3 2xl:px-3 py-1 rounded-full font-google dark:hover:text-white fold:px-2 fold:text-xxs se:text-base sm:text-md md:text-lg lg:text-lg xl:text-xl 2xl:text-xl`}
+                  className={`${currentRoute === link.path ? 'text-black/60 ' : 'text-black/30 '}  transition-colors hover:text-black/60 se:px-2 sm:px-3 md:px-3 lg:px-3 xl:px-3 2xl:px-3 md:py-1 rounded-full font-google sm:py-0.5  fold:px-2 fold:text-xxs se:text-sm sm:text-md md:text-lg lg:text-lg xl:text-xl big:text-xl big:px-3.5`}
                   key={index}
                   href={link.path}
                 >
                   {link.name}
                 </Link>
               ))}
-              <AiOutlineSearch className="dark:text-gray-300 cursor-pointer dark:hover:text-white text-black/30 hover:text-black/60 lg:h-[22px] lg:w-[22px] md:h-[22px] md:w-[22px] sm:h-[20px] sm:w-[20px] se:h-[20px] se:w-[20px]" />
+              <AiOutlineSearch className=" cursor-pointer  text-black/30 hover:text-black/60 big:h-[25px] big:w-[28px] lg:h-[28px] lg:w-[22px] md:h-[22px] md:w-[22px] sm:h-[20px] sm:w-[20px] se:h-[20px] se:w-[20px]" />
             </div>
           </div>
         </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,6 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Navbar from '@/app/components/Navbar'
-import ThemeContextProvider from './context/ThemeContext'
-import ToggleTheme from './components/ToggleTheme'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,12 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr">
-      <body className="dark:bg-black bg-white bg-gradient-to-tr min-h-screen bg-no-repeat from-[#B31942]/20 min-w-screen via-black/10 to-blue-400/30">
-        <ThemeContextProvider>
-          <Navbar />
-          {children}
-          <ToggleTheme />
-        </ThemeContextProvider>
+      <body className=" bg-white bg-gradient-to-tr min-h-screen bg-no-repeat from-[#B31942]/20 min-w-screen via-black/10 to-blue-400/30">
+        <Navbar />
+        {children}
       </body>
     </html>
   )
